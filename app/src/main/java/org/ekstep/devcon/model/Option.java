1606acee0e92729d72e18afe13a8df7367a04d13
Indraja Machani <indraja.machani@tarento.com>
2017-12-11 19:01:33 +0530

    get question based on id and random the questions

---
+++ app/src/main/java/org/ekstep/devcon/model/Option.java
@@ -0,0 +1,26 @@
+package org.ekstep.devcon.model;
+
+/**
+ * Created by Indraja Machani on 12/11/2017.
+ */
+
+public class Option {
+    private int id;
+    private String optionText;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOptionText() {
+        return optionText;
+    }
+
+    public void setOptionText(String optionText) {
+        this.optionText = optionText;
+    }
+}
--- app/src/main/java/org/ekstep/devcon/util/TreasureHuntUtil.java
+++ app/src/main/java/org/ekstep/devcon/util/TreasureHuntUtil.java
@@ -7,10 +7,14 @@ import org.ekstep.devcon.model.QuestionModel;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by Indraja Machani on 12/11/2017.
@@ -44,5 +48,23 @@ public class TreasureHuntUtil {
         Map<String, List<QuestionModel>> treasureMap = new LinkedHashMap<>();
         treasureMap = GsonUtil.fromJson(jsonString, (Type) treasureMap);
         return treasureMap;
+    }
+
+    public static LinkedList<QuestionModel> getRandomQuestionList(String jsonString) {
+        HashMap<String, List<QuestionModel>> treasureMap = GsonUtil.fromJson(jsonString, (Type) new HashMap<>());
+        List<LinkedList<QuestionModel>> valuesList = new ArrayList<>((Collection<?
+                extends LinkedList<QuestionModel>>) treasureMap.values());
+        int index = new Random().nextInt(valuesList.size());
+        return valuesList.get(index);
+    }
+
+    public static QuestionModel getQuestion(String jsonString, int questionId) {
+        LinkedList<QuestionModel> questionList = getRandomQuestionList(jsonString);
+        for (QuestionModel question : questionList) {
+            if (question.hashCode() == questionId) {
+                return question;
+            }
+        }
+        return null;
     }
 }
