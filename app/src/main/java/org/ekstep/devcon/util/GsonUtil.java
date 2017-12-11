c2e2c6edf4985ac952227e1f8d1e07531836b3b0
Indraja Machani <indraja.machani@tarento.com>
2017-12-11 17:14:18 +0530

    Basic model and utils

---
+++ app/src/main/java/org/ekstep/devcon/util/GsonUtil.java
@@ -0,0 +1,50 @@
+package org.ekstep.devcon.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
+/**
+ */
+public class GsonUtil {
+
+    private static Gson sGson;
+
+    public static Gson getGson() {
+        if (sGson == null) {
+            sGson = new GsonBuilder().create();
+        }
+        return sGson;
+    }
+
+    public static <C> C fromJson(String json, Class<C> classOfC) {
+        return getGson().fromJson(json, classOfC);
+    }
+
+    public static <T> T fromJson(String json, Type type) {
+        return (T) getGson().fromJson(json, type);
+    }
+
+    public static <T> T fromJson(String json, Class<T> type, String exceptionMessage) {
+        try {
+            return getGson().fromJson(json, type);
+        } catch (JsonSyntaxException ex) {
+            throw ex;
+        }
+    }
+
+    public static <C> C fromMap(Map map, Class<C> classOfC) {
+        JsonElement jsonElement = GsonUtil.getGson().toJsonTree(map);
+        return getGson().fromJson(jsonElement, classOfC);
+    }
+
+
+    public static String toJson(Object json) {
+        return getGson().toJson(json);
+    }
+
+}
