8c053449902a465754ab96de3deafc251dc38484
Vinayaga Sundar <vinayagasundar93@gmail.com>
2017-12-12 12:30:38 +0530

    Added support for parcelable.

--- app/src/main/java/org/ekstep/devcon/game/models/QuestionModel.java
+++ app/src/main/java/org/ekstep/devcon/game/models/QuestionModel.java
@@ -1,16 +1,55 @@
 package org.ekstep.devcon.game.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by Indraja Machani on 12/11/2017.
  */
 
-public class QuestionModel {
+public class QuestionModel implements Parcelable {
     private String question;
     private int answer;
     private String hint;
     private List<Option> options;
+
+    public QuestionModel() {
+
+    }
+
+    protected QuestionModel(Parcel in) {
+        question = in.readString();
+        answer = in.readInt();
+        hint = in.readString();
+        options = in.createTypedArrayList(Option.CREATOR);
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(question);
+        dest.writeInt(answer);
+        dest.writeString(hint);
+        dest.writeTypedList(options);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<QuestionModel> CREATOR = new Creator<QuestionModel>() {
+        @Override
+        public QuestionModel createFromParcel(Parcel in) {
+            return new QuestionModel(in);
+        }
+
+        @Override
+        public QuestionModel[] newArray(int size) {
+            return new QuestionModel[size];
+        }
+    };
 
     @Override
     public int hashCode() {
