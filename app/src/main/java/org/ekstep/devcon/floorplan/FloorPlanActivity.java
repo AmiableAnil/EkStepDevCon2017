ec7a7b8a13966b799e6a0d41ac097ad942348fed
sneha.krishnamurthy <sneha.krishnamurthy@tarento.com>
2017-12-12 14:25:20 +0530

    Modified Floor plan list screen.

--- app/src/main/java/org/ekstep/devcon/floorplan/FloorPlanActivity.java
+++ app/src/main/java/org/ekstep/devcon/floorplan/FloorPlanActivity.java
@@ -1,10 +1,8 @@
 package org.ekstep.devcon.floorplan;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 import org.ekstep.devcon.R;
 
@@ -12,25 +10,30 @@ import org.ekstep.devcon.R;
  * Created by Sneha on 12/11/2017.
  */
 
-public class FloorPlanActivity extends AppCompatActivity {
-    String[] floorArray = new String[]{"FIRST FLOOR",
-            "SECOND FLOOR", "FOURTH FLOOR", "TREASURE HUNT"};
-    String[] subtitles = new String[]{"Adoption, Reliability, Mobility, Quality",
-            "Agility, Scalability", "Innovation", "Play the game!!"};
-    RecyclerView.LayoutManager layoutManager;
-    private RecyclerView recyclerView;
+public class FloorPlanActivity extends Activity {
+//    String[] floorArray = new String[]{"FIRST FLOOR",
+//            "SECOND FLOOR", "FOURTH FLOOR", "TREASURE HUNT"};
+//    String[] subtitles = new String[]{"Adoption, Reliability, Mobility, Quality",
+//            "Agility, Scalability", "Innovation", "Play the game!!"};
+//    RecyclerView.LayoutManager layoutManager;
+//    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_floor_plan);
 
-        recyclerView = (RecyclerView) findViewById(R.id.rv_floor);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, new FloorPlanFragment())
+                .commit();
 
-        FloorAdapter floorAdapter = new FloorAdapter(floorArray, subtitles);
-        recyclerView.setAdapter(floorAdapter);
+//        recyclerView = (RecyclerView) findViewById(R.id.rv_floor);
+//        recyclerView.setHasFixedSize(true);
+//        layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        FloorAdapter floorAdapter = new FloorAdapter(floorArray, subtitles);
+//        recyclerView.setAdapter(floorAdapter);
     }
 }
