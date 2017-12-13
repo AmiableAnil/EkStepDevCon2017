e8ace8a3902c1c835f3b1b6d47d9adee36f50d76
Anil Kumar Gupta <anil.gupta@tarento.com>
2017-12-13 14:59:58 +0530

    Added FCM setup.

---
+++ app/src/main/java/org/ekstep/devcon/fcm/GenieFirebaseInstanceIDService.java
@@ -0,0 +1,48 @@
+package org.ekstep.devcon.fcm;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created on 7/28/2016.
+ *
+ * @author anil
+ */
+public class GenieFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "GenieFirebaseInstanceIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    /**
+     * Persist token to third-party servers.
+     * <p>
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+        if (!TextUtils.isEmpty(token)) {
+// TODO:           PreferenceUtil.setFcmToken(token);
+        }
+    }
+}
