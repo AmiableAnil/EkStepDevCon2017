3ba44c44173e64bde0446efcdc6bcce418a96e9c
sneha.krishnamurthy <sneha.krishnamurthy@tarento.com>
2017-12-13 16:45:04 +0530

    Telemetry events for treasure hunt game.

--- app/src/main/java/org/ekstep/devcon/telemetry/TelemetryBuilder.java
+++ app/src/main/java/org/ekstep/devcon/telemetry/TelemetryBuilder.java
@@ -6,6 +6,7 @@ import org.ekstep.genieservices.commons.bean.telemetry.Impression;
 import org.ekstep.genieservices.commons.bean.telemetry.Interact;
 import org.ekstep.genieservices.commons.bean.telemetry.Log;
 import org.ekstep.genieservices.commons.bean.telemetry.Telemetry;
+import org.ekstep.genieservices.commons.bean.telemetry.Visit;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,22 +31,21 @@ public class TelemetryBuilder {
         return interact;
     }
 
-    public static Interact buildInteractEvent(InteractionType type, String subType, String pageId, String id, String objectType, Map<String, Object> values, List<CorrelationData> cdata) {
-        List valuesList = new ArrayList();
-        valuesList.add(values);
-        Interact interact = new Interact.Builder().interactionType(type).subType(subType).pageId(pageId).objectId(id).objectType(objectType).values(valuesList).correlationData(cdata).build();
-        return interact;
-    }
-
-    public static Telemetry buildInteractEvent(InteractionType type, String subType, String pageId, Map<String, Object> value) {
+    public static Telemetry buildInteractEvent(InteractionType type, String subType, String pageId, String id, Map<String, String> value) {
         List valuesList = new ArrayList();
         valuesList.add(value);
-        Interact interact = new Interact.Builder().interactionType(type).subType(subType).pageId(pageId).values(valuesList).build();
+        Interact interact = new Interact.Builder().interactionType(type).subType(subType).pageId(pageId).resourceId(id).values(valuesList).build();
         return interact;
     }
 
     public static Telemetry buildImpressionEvent(String pageId, String type, String subType) {
         Impression impression = new Impression.Builder().pageId(pageId).type(type).subType(subType).build();
+        return impression;
+    }
+
+    public static Telemetry buildImpressionEvent(String pageId, String type, String subType, String id, String objType) {
+        Visit visit = new Visit(id, type);
+        Impression impression = new Impression.Builder().pageId(pageId).type(type).subType(subType).addVisit(visit).build();
         return impression;
     }
 }
--- app/src/main/java/org/ekstep/devcon/util/Constant.java
+++ app/src/main/java/org/ekstep/devcon/util/Constant.java
@@ -6,7 +6,7 @@ package org.ekstep.devcon.util;
 
 public interface Constant {
     String PREFS_EKSTEP_DEVCONAPP = "prefs_ekstep_devconapp";
-    String KEY_SET = "key_set";
+    String KEY_SET_VALUE = "key_set";
     String BUNDLE_KEY_SCREEN_NUM = "screen_number";
     String BUNDLE_KEY_STALL_NAME = "stall_name";
 
@@ -19,4 +19,7 @@ public interface Constant {
     String SYNC_MODE_FORCED = "forced";
     String SYNC_TYPE_DEFAULT = "default";
     String DEFAULT_SYNC_TYPE_AND_INTERVAL = "{\"default\":{\"syncInterval\":5, \"mode\":\"auto\"},\"0\":{\"syncInterval\":5, \"mode\":\"forced\"}}";
+    String QUESTION = "que";
+    String EXPECTED_ANSWER = "expectedAnswer";
+    String GIVEN_ANSWER = "givenAnswer";
 }
