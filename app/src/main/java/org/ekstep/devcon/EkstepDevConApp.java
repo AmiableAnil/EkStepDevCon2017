8408e359e635f3fc49a531d34340ff48406955dd
swayangjit <swayangjit029@gmail.com>
2017-12-13 10:50:20 +0530

    Added Genie-Sdk in the app

--- app/src/main/java/org/ekstep/devcon/EkstepDevConApp.java
+++ app/src/main/java/org/ekstep/devcon/EkstepDevConApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import org.ekstep.devcon.util.Constant;
 import org.ekstep.devcon.util.PreferenceUtil;
+import org.ekstep.genieservices.GenieService;
 
 /**
  * @author vinayagasundar
@@ -14,6 +15,7 @@ public class EkstepDevConApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        GenieService.init(this, "org.ekstep.devcon");
         PreferenceUtil.init(this, Constant.PREFS_EKSTEP_DEVCONAPP );
     }
 }
